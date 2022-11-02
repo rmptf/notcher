d50c5bd577cb5282b6440b422812d33eb25d5e93
rmptf <rmpotthoff@example.com>
2022-11-02 09:26:51 -0400

    build: building user role update

--- pages/api/update-user/[...param].js
+++ pages/api/update-user/[...param].js
@@ -1,17 +1,20 @@
 import { ObjectId } from 'mongodb';
-import { dbConnect } from '../../lib/dbConnect';
+import { dbConnect } from '../../../lib/dbConnect';
 
 export default async function handler(req, res) {
   const { db } = await dbConnect();
   try {
-    const { role } = req.body;
+    // const { role } = req.body;
+    const { param } = req.query;
+    console.log('-----------req.body-----------');
+    console.log(req.body);
     // Set dynamically
     const id = '635e8531c6aee5acdc9088ed';
     await db.collection('users').updateOne(
       { _id: ObjectId(id) },
       {
         $set: {
-          role: 'ADMIN',
+          role: param,
         },
       }
     );
@@ -20,15 +23,3 @@ export default async function handler(req, res) {
     res.status(500).send({ error: 'failed to fetch data' });
   }
 }
-
-// fetch('http://localhost:3000/api/auth/session?update').then(
-//   (res) => res.status(200).json('dsfsdfsdfs')
-//   // (res) => res.redirect(307, '/')
-// );
-
-// res.redirect(307, '/');
-
-// res.status(200).json({
-//   data: await db.collection('users').findOne({ _id: ObjectId(id) }),
-//   message: 'Todo updated successfully',
-// });
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 import CatCard from '../components/cards/cat/CatCard';
 import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const { data: session, status } = useSession();
+
+  const role = 'pussy';
 
   function logSession() {
     console.log(session);
@@ -14,7 +17,20 @@ export default function Home() {
     <section className={styles.main}>
       <h1>User Name: {session ? `${session.user.name} ` : 'Guest'}</h1>
       <h1>User Email: {session ? `${session.user.email} ` : 'No User'}</h1>
+      <h1>User Role: {session ? `${session.user.role} ` : 'No User'}</h1>
       <h1>Status: {status}</h1>
+      <button>
+        <Link href={`/api/update-user/USER`}>Change role to User</Link>
+      </button>
+      <br></br>
+      <button>
+        <Link href={`/api/update-user/ADMIN`}>Change role to Admin</Link>
+      </button>
+      <br></br>
+      <button>
+        <Link href={`/api/update-user/${role}`}>Change role dynamically</Link>
+      </button>
+      <br></br>
       <button onClick={logSession}>Log Session</button>
       <h1 className={styles.title}>
         Welcome to <a href="https://nextjs.org">Notcher</a>
